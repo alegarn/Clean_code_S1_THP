bac092ac4f34dccf64a07ded621b1c24793494e7
alegarn <alexgarnier78310@protonmail.com>
2022-08-23 03:05:40 +0200

    contains the given ingredients test

--- spec/legacy_spec.rb
+++ spec/legacy_spec.rb
@@ -32,74 +32,101 @@ RSpec.describe LegacyCodeFromHellYouNeedToTest, crap:"Shit load of crap you were
 
   end
 
-describe Snack do
-  context "?" do
-    # # Snack
-    it 'is shareable returns true' do
-      expect(Snack.new.shareable?).to eq(true)
+  describe Snack do
+    context "there is a Sn.ck" do
+      # # Snack
+      it 'is shareable returns true' do
+        expect(Snack.new.shareable?).to eq(true)
+      end
+
+      it "has no '#brick' method" do
+        expect(Snack.new).to_not respond_to(:brick)
+      end
     end
 
-    it "has no '#brick' method" do
-      expect(Snack.new).to_not respond_to(:brick)
+  end
+
+
+  describe Sandwich < Snack do
+    before(:example) do
+      Sandwich.class_variable_set(:@@count, 0)
+    end
+    context "make a Sandwich.new" do
+      # # Sandwich
+      it 'knows how many have been made so far' do
+        expect(Sandwich.class_variable_defined?("@@count")).to_not eq(nil)
+      end
+
+      it 'can tell that no sanwich has been made so far' do
+        expect(Sandwich.count).to be >= 0
+      end
+
+    end
+
+  end
+
+  context "LegacyCodeFromHellYouNeedToTest" do
+
+
+    before(:example) do
+      LegacyCodeFromHellYouNeedToTest.new
+    end
+
+    # ## Initialize
+    it "doesn't fail when no ingredient is given", LegacyCodeFromHellYouNeedToTest.new() do
+      expect(initialize).to eq(nil)
+    end
+  end
+  context "LegacyCodeFromHellYouNeedToTest" do
+    before(:example) do
+      args = ['ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5']
+    end
+
+    it 'Raises an error when loaded with more than 14 ingredients', args = ['ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5', 'ingredient1', 'ingredient2', 'ingredient3', 'ingredient4, ingredient5'], KARADOC_IDEAL_NUMBER_OF_INGREDIENTS = 14 do
+      expect{Sandwich.new(*args)}.to raise_error.with_message(/Too many ingredients/)
     end
   end
 
-end
-
-
-describe Sandwich < Snack do
+context "Normal Sandwich" do
   before(:example) do
-    Sandwich.class_variable_set :@@count, Sandwich::
+    args = ['ham', 'cheese','ognon']
+    Sandwich.new(*args)
   end
-  context "make a Sandwich.new" do
-    # # Sandwich
-    it 'knows how many have been made so far' do
-      expect(@@count).to_not eq(nil)
-    end
-
-    it 'can tell that no sanwich has been made so far' do
-      excpect(@@count).to eq(0)
-    end
-
+  it 'contains the given ingredients' do
+    expect(Sandwich.ingredients).to eq(['ham', 'cheese','ognon'])
   end
-
 end
 
 
-  # ## Initialize
-  it "doesn't fail when no ingredient is given"
-  it 'Raises an error when loaded with more than 14 ingredients'
-  it 'contains the given ingredients'
+# ## Ingredients
+it 'has ingredients from Ingredients class'
 
-  # ## Ingredients
-  it 'has ingredients from Ingredients class'
+# ## taste
+it 'returns delicious'
 
-  # ## taste
-  it 'returns delicious'
+# ## Pain point
+it 'returns a string when there is at least one tomatoe'
+it 'has no pain point when there is no tomatoe'
 
-  # ## Pain point
-  it 'returns a string when there is at least one tomatoe'
-  it 'has no pain point when there is no tomatoe'
+# ## shareable
+it 'returns a truthy object'
+it 'returns a string depending on the tomatoe situation'
 
-  # ## shareable
-  it 'returns a truthy object'
-  it 'returns a string depending on the tomatoe situation'
+# ## add ingredient
+it 'does nothing when ingredient is not from class Ingredient'
+it 'adds the given ingredient when ingredient is from class Ingredient'
+it 'does not accepts a 14th ingredient nor more'
 
-  # ## add ingredient
-  it 'does nothing when ingredient is not from class Ingredient'
-  it 'adds the given ingredient when ingredient is from class Ingredient'
-  it 'does not accepts a 14th ingredient nor more'
+# ## can be eaten
+it 'can be eaten'
 
-  # ## can be eaten
-  it 'can be eaten'
+# # LegacyCodeFromHellYouNeedToTest
+it 'has two different sandwiches'
 
-  # # LegacyCodeFromHellYouNeedToTest
-  it 'has two different sandwiches'
+# ## 'my sandwich'
+it 'has 3 ingredients'
 
-  # ## 'my sandwich'
-  it 'has 3 ingredients'
-
-  # ## 'your sandwich'
-  it 'has more ingredients than my sandwich'
+# ## 'your sandwich'
+it 'has more ingredients than my sandwich'
 
 end
